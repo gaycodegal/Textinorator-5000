afb7b8e991e9514f43f2c071c8f15b77d039ba52
gaycodegal <8573972+gaycodegal@users.noreply.github.com>
2025-01-15 16:04:34 -0500

    start working on additional font properties

--- js/logic/color-chooser.js
+++ js/logic/color-chooser.js
@@ -1,10 +1,5 @@
 import {colors} from "./colors.js";
 
-function makeShadowColor(color) {
-		const shadow = color=="black" ? "white" : "black";
-		return {fill: color, shadow, shadowBlur: 3};
-}
-
 function selectColor(screen, parent, color, button, selected) {
 		screen.state.focusedColor.set(color);
 		if (selected.selected != null) {
@@ -30,4 +25,25 @@ export function setUpColorChooser(screen, controls) {
 		const selected = {selected: null};
 		const parent = controls.getElementsByClassName("color-choice")[0];
 		colors.map(color=>makeColorButton(screen, parent, color, selected));
+}
+
+
+export function setUpFontColorEvents(screen, controls) {
+		const fontColorFillInput = controls.getElementsByClassName("font-color-fill")[0];
+		const fontColorStrokeInput = controls.getElementsByClassName("font-color-stroke")[0];
+		const fontStrokeStyleInputs = [
+				controls.getElementsByClassName("font-stroke-none")[0]
+				];
+		function doSetFontColor() {
+				let color = {fill: fontColorFillInput.value};
+				
+		}
+		screen.state.focusedFontName.bindListener((name)=>{
+				const isFocused = document.activeElement == fontNameSetter;
+				if (!isFocused && fontNameSetter.value != name) {
+						fontNameSetter.value = name;
+						fontNameSetter.blur();
+				}
+		});
+		fontNameSetter.addEventListener("change", doSetFontName);
 }
