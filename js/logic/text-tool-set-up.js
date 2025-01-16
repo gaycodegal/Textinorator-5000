4ad2538a53329debb240809b5ee6b3b06c200329
gaycodegal <8573972+gaycodegal@users.noreply.github.com>
2025-01-15 21:08:20 -0500

    clean up main a bit

---
+++ js/logic/text-tool-set-up.js
@@ -0,0 +1,23 @@
+import {setUpFontSizeEvents,
+				setUpFontNameEvents,
+				setUpTextChangeEvents,
+				setUpDeleteTextEvent} from "./set-background.js";
+import {setUpFontColorEvents} from "./set-up-color-controls.js";
+import {setUpColorChooser} from "./color-chooser.js";
+
+export function setUpTextToolEvents(textTool, toolControlsElement) {
+		setUpFontSizeEvents(textTool.state.focusedTextSize,
+												toolControlsElement);
+		setUpFontNameEvents(textTool.state.focusedFontName,
+												toolControlsElement);
+		setUpTextChangeEvents(textTool.state.focusedText,
+													toolControlsElement);
+		
+		setUpFontColorEvents(textTool.state.focusedColor,
+												 toolControlsElement);
+		setUpColorChooser(textTool.state.focusedColor,
+											toolControlsElement);
+		
+		setUpDeleteTextEvent(textTool, toolControlsElement);
+}
+
--- js/main.js
+++ js/main.js
@@ -1,42 +1,31 @@
 import * as DrawingCanvas from "./render/canvas.js";
 import {Screen} from "./render/screen.js";
-import {setUpFontSizeEvents, setUpFontNameEvents, setUpTextChangeEvents, setUpBackgroundSetter, setUpDeleteTextEvent, setUpDownloadButton} from "./logic/set-background.js";
-import {setUpFontColorEvents} from "./logic/set-up-color-controls.js";
-import {setUpColorChooser} from "./logic/color-chooser.js";
+import {setUpBackgroundSetter, setUpDownloadButton} from "./logic/set-background.js";
+import {setUpTextToolEvents} from "./logic/text-tool-set-up.js";
 
 export function main() {
 		const canvas = DrawingCanvas.createMainCanvas(document.getElementById("container"));
 		window.canvas = canvas;
-		//const listen = new Listener(canvas);
+
 		canvas.clear();
 
-		const generalControlsElement = document.getElementById("general-controls");		
-		const toolControlsElement = document.getElementById("tool-controls");		
+
 		const screen = new Screen(
 				canvas,
 				/* snap deadzone */ 5,
 				/* handle drag click zone */ 30);
 		window.screen = screen;
 
+		const toolControlsElement = document.getElementById("tool-controls");		
 		const textTool = screen.tools.text;
-		setUpFontColorEvents(textTool.state.focusedColor,
-												 toolControlsElement);
-		setUpFontSizeEvents(textTool.state.focusedTextSize,
-												toolControlsElement);
-		setUpFontNameEvents(textTool.state.focusedFontName,
-												toolControlsElement);
-		setUpTextChangeEvents(textTool.state.focusedText,
-													toolControlsElement);
-		setUpColorChooser(textTool.state.focusedColor,
-											toolControlsElement);
+		setUpTextToolEvents(textTool, toolControlsElement);
 		
-		setUpDeleteTextEvent(textTool, toolControlsElement);
+		const generalControlsElement = document.getElementById("general-controls");
 		setUpBackgroundSetter(screen, generalControlsElement);
 		setUpDownloadButton(screen, generalControlsElement);
-		/*const dragListen = new DragListener(canvas.canvas, canvas.scale, 30, listen);
-			dragListen.bind();*/
 
 		screen.tools.text.strokeText("text here 😊", 0,0);
+
 		//scroll to expose image controls
 		window.scroll(0, 100);
 }
